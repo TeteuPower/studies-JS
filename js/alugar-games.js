b52080d2452a656808f363c5a0850a4f77418763
Matheus <teteupower@gmail.com>
2024-04-06 20:11:32 -0300

    Alugar-Games: Trocando jogos e criando função que aluga e devolve os jogos

--- js/alugar-games.js
+++ js/alugar-games.js
@@ -1,5 +1,26 @@
-
 function sair()
 {    
     window.location.href = '../index.html';
+}
+
+function alterarStatus(id)
+{
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let nomeBotao = jogoClicado.querySelector(".dashboard__item__button");
+
+
+    if(imagem.classList.contains('dashboard__item__img--rented'))
+    {
+        imagem.classList.remove('dashboard__item__img--rented');
+        nomeBotao.textContent = 'Alugar';
+        nomeBotao.classList.remove('dashboard__item__button--return')
+    }
+    else
+    {
+        imagem.classList.add('dashboard__item__img--rented');
+        nomeBotao.textContent = 'Devolver';
+        nomeBotao.classList.add('dashboard__item__button--return');
+    }
+    console.log(nomeBotao);
 }
